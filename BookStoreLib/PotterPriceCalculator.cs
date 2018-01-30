340b8098cd505c1d632e6d384b7f70b13fdbb57e
Jared <QiFen.Pocket@gmail.com>
2018-01-31 00:20:54 +0800

    test pass

--- BookStoreLib/PotterPriceCalculator.cs
+++ BookStoreLib/PotterPriceCalculator.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace BookStoreNS
 {
@@ -14,14 +13,25 @@ namespace BookStoreNS
             int[] array = new int[5];
             shoppingCart.ForEach(book => array[book.Episode - 1]++);
 
+            bool fourFirst = shoppingCart.Count % 4 == 0;
+
             int numberOfDiffEpisode = 0;
 
             do
             {
-                numberOfDiffEpisode = Array.FindAll(array, x => x > 0).Length;
-                array = Array.ConvertAll(array, x => x - 1);
+                numberOfDiffEpisode = 0;
 
-                price += (int) (numberOfDiffEpisode * _potterOff[numberOfDiffEpisode] * _potterPrice);
+                for (int j = 0; j < array.Length; j++)
+                {
+                    if (array[j] > 0)
+                    {
+                        array[j]--;
+                        numberOfDiffEpisode++;
+                        if (fourFirst && numberOfDiffEpisode == 4) break;
+                    }
+                }
+
+                price += (int)(numberOfDiffEpisode * _potterOff[numberOfDiffEpisode] * _potterPrice);
             } while (numberOfDiffEpisode != 0);
 
             return price;
