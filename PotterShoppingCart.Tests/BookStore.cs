d2c134c1d6e11d6562dc073a85850c3b8d8492b7
Jared <QiFen.Pocket@gmail.com>
2018-01-30 22:49:11 +0800

    move book store to other file

---
+++ PotterShoppingCart.Tests/BookStore.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace PotterShoppingCart.Tests
+{
+    public class BookStore
+    {
+        private List<Book> _shoppingCart = new List<Book>();
+        private IPriceCalculator _priceCalculator;
+
+        public BookStore(IPriceCalculator priceCalculator)
+        {
+            _priceCalculator = priceCalculator;
+        }
+
+        public int Checkout()
+        {
+            return _priceCalculator.GetPrice(_shoppingCart);
+        }
+
+        public void PutInShoppingCart(string bookName, int episode, int number)
+        {
+            _shoppingCart.Add(new Book(bookName, episode));
+        }
+
+        public class Book
+        {
+            public Book(string bookName, int episode)
+            {
+                Episode = episode;
+            }
+
+            public int Episode { get; set; }
+        }
+    }
+}
--- PotterShoppingCart.Tests/PotterShoppingCartTest.cs
+++ PotterShoppingCart.Tests/PotterShoppingCartTest.cs
@@ -1,39 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System.Collections.Generic;
 
 namespace PotterShoppingCart.Tests
 {
-    public class BookStore
-    {
-        private List<Book> _shoppingCart = new List<Book>();
-        private IPriceCalculator _priceCalculator;
-
-        public BookStore(IPriceCalculator priceCalculator)
-        {
-            _priceCalculator = priceCalculator;
-        }
-
-        public int Checkout()
-        {
-            return _priceCalculator.GetPrice(_shoppingCart);
-        }
-
-        public void PutInShoppingCart(string bookName, int episode, int number)
-        {
-            _shoppingCart.Add(new Book(bookName, episode));
-        }
-
-        public class Book
-        {
-            public Book(string bookName, int episode)
-            {
-                Episode = episode;
-            }
-
-            public int Episode { get; set; }
-        }
-    }
-
     [TestClass]
     public class PotterShoppingCartTest
     {
