2ac28454c6270daee0da37464fb2e5d87dc54edf
Jared <QiFen.Pocket@gmail.com>
2018-01-30 20:32:10 +0800

    Buy first potter test  fail

--- PotterShoppingCart.Tests/PotterShoppingCartTest.cs
+++ PotterShoppingCart.Tests/PotterShoppingCartTest.cs
@@ -1,1 +1,35 @@
-﻿
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PotterShoppingCart.Tests
+{
+    [TestClass]
+    public class PotterShoppingCartTest
+    {
+        private string _bookPotter = "Potter";
+
+        [TestMethod]
+        public void BuyFirstPotter()
+        {
+            var bookStore = new BookStore();
+
+            bookStore.PutInShoppingCart(_bookPotter, 1, 1);
+            var expected = 100;
+
+            var actual = bookStore.Checkout();
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+
+    public class BookStore
+    {
+        public void PutInShoppingCart(string bookName, int episode, int number)
+        {
+        }
+
+        public int Checkout()
+        {
+            return 0;
+        }
+    }
+}
