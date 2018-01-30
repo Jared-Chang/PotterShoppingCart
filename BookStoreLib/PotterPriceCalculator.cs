7fcb93b8e92126c653a1b3817dc7268e20a2065b
Jared <QiFen.Pocket@gmail.com>
2018-01-31 00:27:30 +0800

    change algorithm

--- BookStoreLib/PotterPriceCalculator.cs
+++ BookStoreLib/PotterPriceCalculator.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace BookStoreNS
 {
@@ -13,28 +15,65 @@ namespace BookStoreNS
             int[] array = new int[5];
             shoppingCart.ForEach(book => array[book.Episode - 1]++);
 
-            bool fourFirst = shoppingCart.Count % 4 == 0;
+            List<List<int>> permutations = GetPermutations(array);
+            List<int> prices = CalculatePrice(permutations);
 
+            return prices.Min();
+        }
+
+        private List<int> CalculatePrice(List<List<int>> permutations)
+        {
+            var result = new List<int>();
+
+            permutations.ForEach(list => { result.Add((int)list.Select(x => x * _potterPrice * _potterOff[x]).Sum()); });
+
+            return result;
+        }
+
+        private List<List<int>> GetPermutations(int[] array)
+        {
+            var result = new List<List<int>>();
+
+            for (int limit = 1; limit < _potterOff.Count; limit++)
+            {
+                result.Add(GetAPermutation(array, limit));
+            }
+
+            return result;
+        }
+
+        private List<int> GetAPermutation(int[] array, int limit)
+        {
+            int[] tempArray = new int[array.Length];
+            Array.Copy(array, tempArray, array.Length);
+            List<int> result = new List<int>();
+
+            var numberOfDiffEpisode = 1;
+
+            while (numberOfDiffEpisode != 0)
+            {
+                numberOfDiffEpisode = GetNumberOfDiffEpisode(limit, tempArray);
+
+                result.Add(numberOfDiffEpisode);
+            }
+
+            return result;
+        }
+
+        private static int GetNumberOfDiffEpisode(int limit, int[] tempArray)
+        {
             int numberOfDiffEpisode = 0;
 
-            do
+            for (int j = 0; j < tempArray.Length && numberOfDiffEpisode != limit; j++)
             {
-                numberOfDiffEpisode = 0;
-
-                for (int j = 0; j < array.Length; j++)
+                if (tempArray[j] > 0)
                 {
-                    if (array[j] > 0)
-                    {
-                        array[j]--;
-                        numberOfDiffEpisode++;
-                        if (fourFirst && numberOfDiffEpisode == 4) break;
-                    }
+                    tempArray[j]--;
+                    numberOfDiffEpisode++;
                 }
+            }
 
-                price += (int)(numberOfDiffEpisode * _potterOff[numberOfDiffEpisode] * _potterPrice);
-            } while (numberOfDiffEpisode != 0);
-
-            return price;
+            return numberOfDiffEpisode;
         }
     }
 }
